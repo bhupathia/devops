1ddbf0661737da8441d2cb4f0006480e01a3b996
Bhupathi <bhupathinaidu.bpn@gmail.com>
2023-08-16 13:24:12 +0530

    users , password added

--- User.sh
+++ User.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "enter username "
-read USERNAME
+read -s USERNAME
 echo "your entered username :$USERNAME"
  echo "enter your password"
- read PASSWORD
+ read -s PASSWORD
  echo "you entered password :$PASSWORD"
