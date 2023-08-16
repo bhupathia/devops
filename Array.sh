a17b03fa774c5785b37d98db25d3197449135642
Bhupathi <bhupathinaidu.bpn@gmail.com>
2023-08-16 13:33:48 +0530

    arrays added

--- Array.sh
+++ Array.sh
@@ -4,5 +4,5 @@ PERSON=("bhupathi","naidu","dhoni")
 
 echo "first person :${PERSON[0]}"
 
-echo "first person :${PERSON[2]}"
-echo "first person :${PERSON[@]}"
+echo "second person :${PERSON[2]}"
+echo "all  :${PERSON[@]}"
